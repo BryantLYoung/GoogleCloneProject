b2c9dbf96ef910e62ab8405f5b8e2e6a876e0e67
Bryant <byoung766363@gmail.com>
2023-04-24 13:26:27 -0700

    3rd commit

--- pages/api/HeaderOptions.js
+++ pages/api/HeaderOptions.js
@@ -14,7 +14,7 @@ function HeaderOptions() {
             <div className="flex space-x-6">
                 <HeaderOption Icon={SearchIcon} title="All" selected />
                 <HeaderOption Icon={PhotographIcon} title="Images" />
-                <HeaderOption Icon={PlayIcon} title="Videos" selected />
+                <HeaderOption Icon={PlayIcon} title="Videos" />
                 <HeaderOption Icon={NewspaperIcon} title="News" />
                 <HeaderOption Icon={MapIcon} title="Maps" />
                 <HeaderOption Icon={DotsVerticalIcon} title="More" />
